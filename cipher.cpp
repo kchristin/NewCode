0207c4e9741b9d25c0fa4afee3f0e5a8724c4209
kchristin <christingeorge321@gmail.com>
2021-10-27 18:15:19 -0400

    update

--- cipher.cpp
+++ cipher.cpp
@@ -10,5 +10,19 @@ int main(){
     std::cout << "Enter plain one word message (A-Z only) : " ;
     std::cin >> S;
 
+    char C[S.size() + 1];
+    S.copy(C, S.size() + 1);
+    
+    for (int i = 0; i < S.size(); i++){
+        if(static_cast<int>(C[i] + N) >= 65 && static_cast<int>(C[i] + N) <= 90){
+            std::cout << char(C[i] + N);
+        }
+        else{
+            int F = static_cast<int>(C[i] + N) - 90;
+            std::cout << char(65 + (F - 1));
+        }
+    }
+    std::cout << ' ' << std::endl;
+
     return 0;
 }
