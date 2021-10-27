2b90d41d01ae50a8b472d63536643f9777a64357
kchristin <christingeorge321@gmail.com>
2021-10-27 18:19:04 -0400

    update

--- cipher.cpp
+++ cipher.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main(){
 
-    std::string S;
+    string S;
     int N;
-    std::cout << "Enter shift +/- 26 : ";
-    std::cin >> N;
-    std::cout << "Enter plain one word message (A-Z only) : " ;
-    std::cin >> S;
+    cout << "Enter shift +/- 26 : ";
+    cin >> N;
+    cout << "Enter plain one word message (A-Z and a-z only) : " ;
+    cin >> S;
 
     char C[S.size() + 1];
     S.copy(C, S.size() + 1);
-    
+
     for (int i = 0; i < S.size(); i++){
         if(static_cast<int>(C[i] + N) >= 65 && static_cast<int>(C[i] + N) <= 90){
-            std::cout << char(C[i] + N);
+            cout << char(C[i] + N);
         }
         else{
             int F = static_cast<int>(C[i] + N) - 90;
-            std::cout << char(65 + (F - 1));
+            cout << char(65 + (F - 1));
         }
     }
-    std::cout << ' ' << std::endl;
+    cout << ' ' << endl;
 
     return 0;
 }
