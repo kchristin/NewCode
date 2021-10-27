43679beb6dae4531f3f4430995bdfd66bfa2f91f
kchristin <christingeorge321@gmail.com>
2021-10-27 18:04:54 -0400

    update

--- cipher.cpp
+++ cipher.cpp
@@ -2,6 +2,13 @@
 #include <string>
 
 int main(){
-    
+
+    std::string S;
+    int N;
+    std::cout << "Enter shift +/- 26 : ";
+    std::cin >> N;
+    std::cout << "Enter plain one word message (A-Z only) : " ;
+    std::cin >> S;
+
     return 0;
 }
