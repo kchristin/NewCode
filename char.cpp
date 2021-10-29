d31d4ba9fd695d70c4e2858d9206a5d35795a9c5
kchristin <christingeorge321@gmail.com>
2021-10-28 21:14:07 -0400

    Update char.cpp

--- char.cpp
+++ char.cpp
@@ -3,7 +3,50 @@
 
 using namespace std;
 
-int main(){
+const int N = 120;
 
+char generate_random_char()
+{
+    int i = 26.0 * rand() / RAND_MAX;
+
+    if (static_cast<double>( rand() ) / RAND_MAX  < 0.5) {
+        return static_cast<char>( 65+i );
+    }
     
+    return static_cast<char>( 97+i );
+}
+
+int main()
+{
+    srand(time(0));
+
+    // Set up an array of characters
+    char arr[N];
+    for (int i=0; i<N; ++i) {
+        arr[i] = generate_random_char();
+    }
+
+    // Printing the contents of the input array
+    cout << "Input array:" << endl;
+    for (int i=0; i<N; ++i) {
+        cout << arr[i];
+    }
+    cout << endl;
+
+    // TO DO 
+    int num = 0;
+    cout << "counts:" << endl;
+    for (int j = 65; j <= 90; j++) {
+        for (int i = 0; i < N; i++){
+            if ((arr[i] == static_cast<char>(j)) || (arr[i] == static_cast<char>((j + 32))))
+            {
+                num++;
+            }
+        }
+        cout << static_cast<char>(j) << ":" << num << endl;
+        num = 0;      
+    }
+    cout << "Total number of charecters : " << N << endl; 
+
+    return 0;
 }
