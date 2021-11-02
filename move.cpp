ca5add16d663f3ab22d2914904b652ed736fc1d1
kchristin <christingeorge321@gmail.com>
2021-11-01 22:36:42 -0400

    upgrade

--- move.cpp
+++ move.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main() {
 
-int N[5] = {0, 10, 15, 0, 2};
+int N[5] = {0, 10, 15, 0, 0};
 
 for (int i = 0; i < 5; i++){
-    if (N[i] == 0){
+    if (N[i] == 0 && i != 4){
         N[i] = N[i+1];
         N[i+1] = 0;
+    }
+    else if (N[i] != 0){
+        N[i] = N[i-2];
     }
 }
 for (int i = 0; i < 5; i++) {
