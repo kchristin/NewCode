42e0eb1d28c7be6f1ddea8c8266d999c80610fcb
kchristin <christingeorge321@gmail.com>
2021-11-02 22:02:26 -0400

    Update

--- move.cpp
+++ move.cpp
@@ -1,14 +1,24 @@
 //Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-int N[5] = {0, 10, 15, 0, 0};
+int S; // Number of array
+cout << "Input the number of Array" << endl;
+cin >> S; // S is the number of arry
 
-for (int i = 0; i < 5; i++){
-    if (N[i] == 0 && i != 4){
+int N[S] = {};
+cout << "Input the numbers in Array" << endl;
+
+for (int i = 0; i < S; i++){
+cin >> N[i];
+}
+
+for (int i = 0; i < S; i++){
+    if (N[i] == 0 && i != (S-1)){
         N[i] = N[i+1];
         N[i+1] = 0;
     }
@@ -16,7 +26,7 @@ for (int i = 0; i < 5; i++){
         N[i] = N[i-2];
     }
 }
-for (int i = 0; i < 5; i++) {
+for (int i = 0; i < S; i++) {
 cout << N[i] << endl; }
 
 }
