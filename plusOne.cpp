dbba1faf4ec3f450e5499f918e9a9c9f5d443c1a
kchristin <christingeorge321@gmail.com>
2021-11-03 22:09:53 -0400

    Create plusOne.cpp

---
+++ plusOne.cpp
@@ -0,0 +1,18 @@
+//66. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+// The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+//Increment the large integer by one and return the resulting array of digits.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int i = 0;
+    int N[] = {};
+    cout << "Input : digits = " ;
+    cin >> N[i]; i ++;
+    cout << N[0] << endl;
+
+
+}
