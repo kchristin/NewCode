8e838a51137fcf171fa1071b8b6781119f2c826b
kchristin <christingeorge321@gmail.com>
2022-03-08 12:35:34 -0500

    merging two lists
    back to square 1

---
+++ MergeTwoList.cpp
@@ -0,0 +1,26 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){ 
+
+    int list1[3] = {};
+    for (int i = 0; i< 3; i++){
+        cin >> list1[i];
+    }
+    int list2[3] = {};
+    for (int i = 0; i< 3; i++){
+        cin >> list1[i];
+    }
+    for (int i = 0; i< 3; i++){
+        cout << list1[i];
+    }        
+    for (int i = 0; i< 3; i++){
+        cout << list2[i];
+    }
+
+}
