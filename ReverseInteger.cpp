b9fb81bd8e62dc4b5faafa04f2d77dcc1c81613a
kchristin <christingeorge321@gmail.com>
2022-03-20 11:21:06 -0400

    Create ReverseInteger.cpp

---
+++ ReverseInteger.cpp
@@ -0,0 +1,14 @@
+// Given a signed 32-bit integer x, return x with its digits reversed.
+// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){ 
+    
+    int n;
+    cout <<"Enter some numbers" << endl;
+    cin >> n;
+
+}
