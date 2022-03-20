0d0c272a52afb1637d57d821e4c8f44346b76cc5
kchristin <christingeorge321@gmail.com>
2022-03-20 11:29:56 -0400

    Create DivideIntegers.cpp

---
+++ DivideIntegers.cpp
@@ -0,0 +1,16 @@
+//Given two integers dividend and divisor, divide two integers.
+// The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8.
+
+#include <iostream>
+using namespace std;
+
+int main(){ 
+
+    int Div;
+    cout <<"Enter the Divident" << endl;
+    cin >> Div;
+
+    int Dis;
+    cout <<"Enter the Divisor" << endl;
+    cin >> Dis;
+}
