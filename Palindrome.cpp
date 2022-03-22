ba0a8f79b965fd3a3c037f1b1df5c7a5b5f99117
kchristin <christingeorge321@gmail.com>
2022-03-22 10:20:00 -0400

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,14 @@
+// Given an integer x, return true if x is palindrome integer.
+// An integer is a palindrome when it reads the same backward as forward.
+
+#include <iostream>
+using namespace std;
+
+int main(){ 
+
+    int num;
+    cout <<"Enter a number" << endl;
+    cin >> num;
+
+    if 
+}
