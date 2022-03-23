c50f374f22ec1265aa39938d784cf6b40fd0c54d
kchristin <christingeorge321@gmail.com>
2022-03-23 10:50:24 -0400

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,14 +1,18 @@
 // Given an integer x, return true if x is palindrome integer.
 // An integer is a palindrome when it reads the same backward as forward.
+// For example, 121 is a palindrome while 123 is not.
+// so, the soulution is to divide the number in half, if it is even to compare to the other half 
+// or get rid of the middle number and following the same procedure if it is odd.
 
 #include <iostream>
+
 using namespace std;
 
-int main(){ 
+int main() {
 
-    int num;
-    cout <<"Enter a number" << endl;
-    cin >> num;
+    int x
+    cout << "Enter a number" << endl; 
+    cin >> x;
 
-    if 
+    
 }
