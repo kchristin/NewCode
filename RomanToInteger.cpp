25c462f3bb407060814745e27c3d537cef336068
kchristin <christingeorge321@gmail.com>
2022-03-28 10:19:11 -0400

    Update RomanToInteger.cpp
    Updated

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -13,6 +13,26 @@ int main() {
     cout << "Enter a Roman Numeral" << endl;
     cin >> x;
 
-    
+    if (x == I) {
+        cout << "Output = " << I << endl;
+    }
+    if (x == V) {
+        cout << "Output = " << V << endl;
+    }
+    if (x == X) {
+        cout << "Output = " << X << endl;
+    }
+    if (x == L) {
+        cout << "Output = " << L << endl;
+    }
+    if (x == C) {
+        cout << "Output = " << C << endl;
+    }
+    if (x == D) {
+        cout << "Output = " << D << endl;
+    }
+    if (x == M) {
+        cout << "Output = " << M << endl;
+    }
 
 }
