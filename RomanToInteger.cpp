6e7dfb5ad46fa2a978e13f956f24cad6d632cfb0
kchristin <christingeorge321@gmail.com>
2022-03-28 10:12:30 -0400

    Create RomanToInteger.cpp

---
+++ RomanToInteger.cpp
@@ -0,0 +1,18 @@
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+// Given a roman numeral, convert it to an integer. I - 1,V - 5,X - 10,L  - 50,C - 100,D - 500,M - 1000
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000;
+    
+    int x;
+    cout << "Enter a Roman Numeral" << endl;
+    cin >> x;
+
+    
+
+}
