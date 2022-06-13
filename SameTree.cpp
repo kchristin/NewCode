ef93ca8ec78bf8c6b50b131262cd46d6f540c9a7
kchristin <christingeorge321@gmail.com>
2022-06-13 13:00:24 -0400

    Update SameTree.cpp
    makes two binary trees and compare them and display output if they are same or not

--- SameTree.cpp
+++ SameTree.cpp
@@ -5,6 +5,31 @@
 using namespace std;
 
 int main() {
-    
 
+    int N;
+
+    cout <<"Enter the number of nodes you want in both Binary trees P and Q" << endl;
+    cin >> N;
+    int P[N] = {};
+    int Q[N] = {};
+    int T;
+    
+    cout <<"Enter the numbers you want on binary tree P" << endl;
+    for (int i = 0; i < N; i++){
+        cin >> T;
+        P[i] = T;
+    }
+    cout <<"Enter the numbers you want on binary tree Q" << endl;
+    for (int i = 0; i < N; i++){
+        cin >> T;
+        Q[i] = T;
+    }
+
+    if (P == Q)
+        cout <<"Both binary trees are the same." << endl;
+    else
+        cout <<"Both binary trees are not the same." << endl;
+    
+    return 0;
+        
 }
